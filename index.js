691ad333a35a83c6fe21e6dd0a1ac5534b4330e4
Gaurav <gaurav@gauravkulkarni.com>
2017-10-22 10:43:06 -0700

    Fetch raw crossword json

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const rp = require("request-promise");
 const app = express();
 
 app.get("/", function(req, res) {
   res.send("Dat Crossword Site");
+});
+
+app.get("/:year-:month-:day", function(req, res) {
+  const { year, month, day } = req.params;
+  const url = `https://www.nytimes.com/svc/crosswords/v2/puzzle/daily-${year}-${month}-${day}.json`
+  const jar = rp.jar();
+  const cookie = rp.cookie(`NYT-S=1owNgcqzHieibU6FN9lbz4EsZq6W84oCOfLmaGjrBuSW7JrIojy4HW5bbeKpPyM.8isIBqltGHSamwobwO1clL9xvTvNSIWusuWRr6OkbThsLlfp5eLfv6Mg00`);
+  jar.setCookie(cookie, url)
+  rp({ url, jar, json: true }).then((jsonResp) => {
+    const puzz = jsonResp.results[0].puzzle_data;
+    res.send(JSON.stringify(puzz.layout, null, 2));
+  });
 });
 
 const port = process.env.PORT || 8080;
