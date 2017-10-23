f89f3a05eb6f5ca64a150e7de880f6779893ee57
Gaurav <gaurav@gauravkulkarni.com>
2017-10-23 09:56:17 -0700

    Get rules in

--- public/crossword.js
+++ public/crossword.js
@@ -1,14 +1,52 @@
 const squareSize = 40; // in px
 
-function createEmptyCrossword() {
+function drawGrid() {
   const size = Math.sqrt(puzzData.layout.length);
   for (i = 0; i < size; i++) {
     for (j = 0; j < size; j++) {
-      const elt = $("<div>", {"class": "puzzSquare", style: `top: ${squareSize * i}px; left: ${squareSize * j}px`});
-      if (puzzData.layout[i * size + j] == 0) {
+      const squareNum = i * size + j;
+      const eltProps = {
+        "data-i": i,
+        "data-j": j,
+        "data-squareNum": squareNum,
+        "class": "puzzSquare",
+      }
+      const elt = $("<div>", eltProps);
+      if (puzzData.layout[squareNum] == 0) {
         elt.addClass("black");
       }
       $("#puzzContainer").append(elt);
+      $("#puzzContainer").css({ width: `${(squareSize + 2) * size}px` });
     }
   }
+
+  // Grab clueNums to populate the little numbers
+  const clueNums = {}
+  puzzData.clues.A.forEach(e => clueNums[e.clueNum] = e.clueStart)
+  puzzData.clues.D.forEach(e => clueNums[e.clueNum] = e.clueStart)
+
+  for (key in clueNums) {
+    val = clueNums[key];
+    const square = $(`div[data-squareNum='${val}']`);
+    const littleNum = $("<div>", {"class": "clueNum"});
+    littleNum.html(key);
+    square.append(littleNum);
+  }
+}
+
+function drawRules() {
+  ['A', 'D'].forEach((dir) => {
+    const ruleCol = $("<div>", {"class": "ruleCol"});
+    puzzData.clues[dir].forEach(rule => {
+      const ruleElt = $("<div>", {"class": "ruleRow"});
+      ruleElt.html(`${rule.clueNum}: ${rule.value}`);
+      ruleCol.append(ruleElt);
+    });
+    $("#puzzRules").append(ruleCol);
+  });
+}
+
+function createEmptyCrossword() {
+  drawGrid();
+  drawRules();
 }
